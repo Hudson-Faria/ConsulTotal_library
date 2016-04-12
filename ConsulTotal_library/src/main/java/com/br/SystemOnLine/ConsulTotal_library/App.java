2c94c907d0858f233db83c6f8bfeb3e1f65ad859
Hudson Faria <hudson.lfaria@gmail.com>
2016-04-12 16:54:38 -0300

    Initial Project

---
+++ ConsulTotal_library/src/main/java/com/br/SystemOnLine/ConsulTotal_library/App.java
@@ -0,0 +1,13 @@
+package com.br.SystemOnLine.ConsulTotal_library;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
